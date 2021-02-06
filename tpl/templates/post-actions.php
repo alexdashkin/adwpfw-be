a99472cbaa90d762c513f5c7a3b0dab87e6004ef
Alex Dashkin <alex.dashkin@gmail.com>
2021-02-06 17:07:27 +0300

    Fix empty option value

--- tpl/templates/post-actions.php
+++ tpl/templates/post-actions.php
@@ -1,6 +1,6 @@
 <select name="<?= $name ?>>">
 
-    <option value="0">All <?= ucfirst($label) ?></option>
+    <option value="">All <?= ucfirst($label) ?></option>
 
     <?php foreach ($options as $option): ?>
         <option value="<?= $option['value'] ?>" <?= $option['selected'] ?>><?= $option['label'] ?></option>
